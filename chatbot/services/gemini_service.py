2cb1abedca389e0175fcd38cd42104efad4704f5
Anurag7376 <bhartianurag240@gmail.com>
2026-02-15 20:43:49 +0530

    ohh god... please run this time

--- chatbot/services/gemini_service.py
+++ chatbot/services/gemini_service.py
@@ -1,21 +1,19 @@
 import os
-from google import genai
+import google.generativeai as genai
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 if not GEMINI_API_KEY:
     raise ValueError("GEMINI_API_KEY not set.")
 
-client = genai.Client(api_key=GEMINI_API_KEY)
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 
 def generate_ai_response(prompt: str):
     try:
-        response = client.models.generate_content(
-            model="gemini-1.5-flash",
-            contents=prompt,
-        )
-
+        response = model.generate_content(prompt)
         return response.text
 
     except Exception as e:
